751453721b61b8cfd2610ed17f7bcd95c148fa57
MJP8 <malachi@timelapseplus.com>
2021-04-07 11:49:33 -0500

    fixed some bugs (#7 and #5) and rounded corners of sprite

--- game.js
+++ game.js
@@ -5,10 +5,10 @@ async function pause(ms) {
     await sleep(ms);
 }
 class Sprite {
-    constructor(width = '10px', height = '10px', x = '50%', y = '0%', color = '#000', speed = 1000) {
-        this.speed = speed;
+    constructor(width = '10px', height = '10px', x = '50%', y = '0%', color = '#000') {
         this.x = x;
         this.y = y;
+        this.test = parseInt(width) / 2;
         this.createElement = function() {
             let elem = $('<div id="sprite"></div>');
             elem.css('width', width);
@@ -17,13 +17,14 @@ class Sprite {
             elem.css('left', this.x);
             elem.css('top', this.y);
             elem.css('background', color);
+            elem.css('border-radius', (this.test + 'px'));
             $('.game').append(elem);
         }
     }
 }
 class Enemy extends Sprite {
-    constructor(visibility = 1, lives = 1, speed = 1000) {
-        super('10px', '10px', '50%', '0%', '#000', speed);
+    constructor(visibility = 1, lives = 1) {
+        super('20px', '20px', '50%', '0%', '#000');
         this.lives = lives;
         this.visibility = visibility;
         this.moveToX = Math.round(Math.random() * $(window).width());
@@ -38,29 +39,23 @@ class Enemy extends Sprite {
         }
         this.move = function() {
             $('#sprite').addClass('enemy');
-            $('.enemy').animate({ left: '+=' + this.moveToX + 'px', top: '+=' + this.moveToY + 'px' }, 'fast');
+            $('.enemy').animate({ left: this.moveToX + 'px', top: this.moveToY + 'px' }, 400);
         }
     }
 }
 enemy = new Enemy();
 enemy.createElement();
 enemy.checkVisibility();
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < 1000; i++) {
     enemy.move();
     let text_object = {
-        left: $('.enemy').css('left'),
-        top: $('enemy').css('top'),
         width: $('.enemy').css('width'),
         height: $('.enemy').css('height')
     }
-    text_object.left.replace('px', '');
-    text_object.left = parseInt(text_object.left);
-    text_object.top.replace('px', '');
-    text_object.top = parseInt(text_object.top);
-    text_object.width.replace('px', '');
+    text_object.width.replace('px', '')
     text_object.width = parseInt(text_object.width);
     text_object.height.replace('px', '');
     text_object.height = parseInt(text_object.height);
-    enemy.moveToX = Math.round(Math.random() * (($(window).width() - text_object.left) - text_object.width));
-    enemy.moveToY = Math.round(Math.random() * (($(window).height() - text_object.top) - text_object.height));
+    enemy.moveToX = Math.round(Math.random() * ($(window).width() - text_object.width));
+    enemy.moveToY = Math.round(Math.random() * ($(window).height() - text_object.height));
 }
